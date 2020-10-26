4fcabf270038e8b55cb62840249bc7db63b66787
choisungwook <kgg1959@naver.com>
2020-10-26 19:43:01 +0900

    Change 페이지 인증: 모두접근 허용

--- src/main/java/SpringSecurity/Login/security/SecurityConfig.java
+++ src/main/java/SpringSecurity/Login/security/SecurityConfig.java
@@ -20,8 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .httpBasic();
     }
