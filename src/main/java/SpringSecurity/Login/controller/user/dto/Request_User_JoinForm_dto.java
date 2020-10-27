9424e0726120373bd79711458b575821b3512f62
choisungwook <kgg1959@naver.com>
2020-10-27 12:34:36 +0900

    Change 회원가입 요청에 Role추가

--- src/main/java/SpringSecurity/Login/controller/user/dto/Request_User_JoinForm_dto.java
+++ src/main/java/SpringSecurity/Login/controller/user/dto/Request_User_JoinForm_dto.java
@@ -1,6 +1,7 @@
 package SpringSecurity.Login.controller.user.dto;
 
 import SpringSecurity.Login.user.DBUser;
+import SpringSecurity.Login.user.role.UserRole;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,14 @@ public class Request_User_JoinForm_dto {
     private String username;
     private String email;
     private String password;
+    private UserRole role;
 
     @Builder
-    public Request_User_JoinForm_dto(String username, String email, String password) {
+    public Request_User_JoinForm_dto(String username, String email, String password, UserRole role) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
     public DBUser toEntity(){
