f8ffc917275230847e8e908ae508a1d22bd187ff
choisungwook <kgg1959@naver.com>
2020-10-28 16:09:06 +0900

    Change 커스텀 인증필터 삭제

--- src/main/java/SpringSecurity/Login/security/SecurityConfig.java
+++ src/main/java/SpringSecurity/Login/security/SecurityConfig.java
@@ -1,17 +1,16 @@
 package SpringSecurity.Login.security;
 
 import SpringSecurity.Login.security.jwt.JwTokenVerifier;
-import SpringSecurity.Login.security.jwt.JwtEmailAndPasswordAuthenticationFilter;
-import SpringSecurity.Login.user.role.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import static SpringSecurity.Login.user.role.UserRole.ADMIN;
 
 @Configuration
@@ -24,13 +23,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public JwTokenVerifier jwTokenVerifier(){
+        return new JwTokenVerifier();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(new JwtEmailAndPasswordAuthenticationFilter(authenticationManager()))
-                .addFilterAfter(new JwTokenVerifier(), JwtEmailAndPasswordAuthenticationFilter.class)
+                .addFilterBefore(jwTokenVerifier(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                     .antMatchers("/api/v1/book/write").hasRole(ADMIN.name())
                 .anyRequest().permitAll();
